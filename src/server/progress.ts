aacdf89c27c30f3d1c4037f74e3e3a612c2372b8
Joshua Castle <26531652+Kas-tle@users.noreply.github.com>
2024-03-24 16:26:19 -0700

    Formatting fixes
    Signed-off-by: Joshua Castle <26531652+Kas-tle@users.noreply.github.com>

--- src/server/progress.ts
+++ src/server/progress.ts
@@ -4,9 +4,11 @@ import figures from 'figures';
 
 export function defaultBar(): cliProgress.SingleBar {
     return new cliProgress.SingleBar({
-        format: chalk.green(figures.bullet) + chalk.green(' og images ') + '{bar}' + chalk.white(' {prefix} ({percentage}%) ') + chalk.grey('{value}/{total} ') + chalk.grey('{suffix}'),
+        format: chalk.green(figures.bullet) + chalk.green(' og images ') + '{bar}' + chalk.whiteBright(' {prefix} ({percentage}%) ') + chalk.grey('{value}/{total} ') + chalk.grey('{suffix}'),
         barCompleteChar: chalk.green('█'),
-        barIncompleteChar: chalk.white('█'),
-        hideCursor: true
+        barIncompleteChar: chalk.whiteBright('█'),
+        hideCursor: true,
+        barsize: 250,
+        clearOnComplete: true
     });
 };
